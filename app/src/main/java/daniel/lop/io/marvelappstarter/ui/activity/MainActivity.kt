31ef5dd0dfc912c2cc587d9cec1c38cceebc7144
ruanhcr <ruanhenriqueruiz@gmail.com>
2021-12-22 11:04:34 -0300

    Initial commit

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/ui/activity/MainActivity.kt
@@ -0,0 +1,34 @@
+package daniel.lop.io.marvelappstarter.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+import daniel.lop.io.marvelappstarter.R
+import daniel.lop.io.marvelappstarter.databinding.ActivityMainBinding
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView(binding)
+    }
+
+    private fun initView(binding: ActivityMainBinding) {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {  }
+        }
+    }
+}
